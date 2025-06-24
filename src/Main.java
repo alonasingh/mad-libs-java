4b6910a622482605cbbd02a50092397d29866872
Alona Singh <alonasingh@outlook.com>
2025-06-24 13:20:27 +0530

    Initial commit - Shopping Cart Java program

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,31 @@
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //MAD LIBS GAME
+         //SHOPPING CART PROGRAM
 
         Scanner scanner = new Scanner(System.in);
 
-        String adjective1;
-        String noun1;
-        String adjective2;
-        String verb1;
-        String adjective3;
+        String item;
+        double price;
+        int quantity;
+        String currency = "$";
+        double total;
 
-        System.out.println("Enter adjective1 : ");
-        adjective1 = scanner.nextLine();
-        System.out.println("Enter noun : ");
-        noun1 = scanner.nextLine();
-        System.out.println("Enter adjective2 : ");
-        adjective2 = scanner.nextLine();
-        System.out.println("Enter verb : ");
-        verb1 = scanner.nextLine();
-        System.out.println("Enter adjective3 : ");
-        adjective3 = scanner.nextLine();
+        System.out.println("Item to buy: ");
+        item = scanner.nextLine();
+        System.out.println("Enter the price: ");
+        price = scanner.nextDouble();
+        System.out.println("What is the quantity: ");
+        quantity = scanner.nextInt();
 
-        System.out.println("Today I went to an " + adjective1 + " office");
-        System.out.println("At the office, I learnt " + noun1 + "." );
-        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
-        System.out.println("At the end of the workday I was " + adjective3 + ".");
+        total = price * quantity;
 
+        System.out.println("\n You have bought " + quantity + " " + item + "/s");
+        System.out.println("Your total is " + currency + total);
         scanner.close();
-
 
 
         }
